8f906e2c1a872cf22a98ea10dbd9e8e54669066b
Thomas Spooner <tspooner.dev@gmail.com>
2025-12-11 14:32:44 -0600

    Reafactored the navbar so it doesn't display on /studio (CMS).

--- src/components/navbar-wrapper.tsx
+++ src/components/navbar-wrapper.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
+import { useBanner } from './banner-context'
 import { Container } from './container'
 import { Navbar } from './navbar'
-import { useBanner } from './banner-context'
 
 export function NavbarWrapper() {
   const banner = useBanner()
+  const pathname = usePathname()
+
+  // Hide navbar on CMS studio route
+  if (pathname?.startsWith('/studio')) {
+    return null
+  }
 
   return (
     <Container>
